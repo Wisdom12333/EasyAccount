9a4352769da0de97fb081c526335a0bbace82116
Wisdom12333 <873842869@qq.com>
2021-12-03 11:01:33 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <el-container class="el-container">
-      <el-header height="30px" style="background: #938989">Hi</el-header>
+      <el-header height="30px" 
+      style="background: #938989">Hi</el-header>
       <el-container>
-        <el-aside width="300px" style="background: #d7c3c3">Aside</el-aside>
+        <el-aside width="300px" 
+        style="background: #d7c3c3">Aside</el-aside>
         <el-main style="background: #99f8ff">
           <router-view />
         </el-main>
