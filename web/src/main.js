44ffcb55fcaa8b22735fce0fa7f89c83a505ba26
shirj <shirj@asiainfo.com>
2021-12-22 17:53:11 +0800

    Edit Register.vue

--- web/src/main.js
+++ web/src/main.js
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "./plugins/element";
 import store from "./store";
+import axios from 'axios';
 
-createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
+const app = createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
+app.config.globalProperties.$http=axios;
--- web/src/views/Register.vue
+++ web/src/views/Register.vue
@@ -1,14 +1,46 @@
 <template>
-  <div>Register</div>
-  <div>sum的值是 {{ $store.state.sum }}</div>
+  <h1>Register</h1>
+  <br />
+  <form>
+    <label>
+      用户名<input placeholder="input" v-model="user.username" />
+    </label>
+    <label>
+      密码<input type="password" v-model="user.password" />
+    </label>
+    <label>
+      确认密码<input type="password" v-model="user.checkPwd" />
+    </label>
+    <label>
+      邮箱<input v-model="user.eMail" />
+    </label>
+    <label>
+      <button @click.prevent="submit()">提交</button>
+    </label>
+  </form>
 </template>
 
 <script>
 export default {
   name: "Register",
   data() {
-    return {};
+    return {
+      user: {
+        username: "",
+        password: "",
+        checkPwd: "",
+        eMail: ""
+      }
+    };
   },
+  methods:{
+    submit(){
+      console.log(this.user);
+      this.axios.get("localhost:8080/user").then((response) => {
+        console.log(response.data)
+      })
+    },
+  }
 };
 </script>
 
