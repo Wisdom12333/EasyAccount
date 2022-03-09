5927d6128d4887b98cda61003b7fa4b6676a1277
Wisdom12333 <873842869@qq.com>
2022-03-09 17:24:31 +0800

    Use element-UI on Register.vue.

--- web/src/views/Register.vue
+++ web/src/views/Register.vue
@@ -1,27 +1,36 @@
 <template>
   <h1>Register</h1>
   <br />
-  <form>
-    <label> 用户名<input v-model="user.username" placeholder="input" /> </label>
-    <label> 密码<input v-model="user.password" type="password" /> </label>
-    <label> 确认密码<input v-model="user.checkPwd" type="password" /> </label>
-    <label> 邮箱<input v-model="user.eMail" /> </label>
-    <label>
-      <button @click.prevent="submit()">提交</button>
-    </label>
-    <label>
-      <router-link to="/Login">已有账号？现在登陆</router-link>
-    </label>
-  </form>
+  <el-form :model="user" label-width="100px" style="width: 50%">
+    <el-form-item label="用户名">
+      <el-input v-model="user.username" placeholder="请输入用户名" />
+      <el-tag v-show="usernameCheck===false" type="danger">用户名已被使用!</el-tag>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="user.password" type="password" />
+    </el-form-item>
+    <el-form-item label="确认密码">
+      <el-input v-model="user.checkPwd" type="password" />
+    </el-form-item>
+    <el-form-item label="邮箱">
+      <el-input v-model="user.eMail" />
+    </el-form-item>
+    <el-form-item>
+      <el-space :size="10" spacer="|">
+        <el-button type="primary" @click.prevent="submit()">提交</el-button>
+        <el-check-tag checked  @click="router.push({ name: 'Login' })">已有账号？现在登陆!</el-check-tag>
+      </el-space>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script setup>
 import axios from "axios";
-import { useRouter } from "vue-router";
-import { reactive, watch } from "vue";
+import {useRouter} from "vue-router";
+import {reactive, ref, watch} from "vue";
 
 const router = useRouter();
-
+let usernameCheck = ref(true);
 const user = reactive({
   username: undefined,
   password: undefined,
@@ -45,8 +54,8 @@ watch(
   () => user.username,
   (val) => {
     axios.get("/user/checkUsername?username=" + val).then(
-      () => console.log("可用"),
-      () => console.log("用户名已被使用")
+      () => usernameCheck.value = true,
+      () => usernameCheck.value = false,
     );
   }
 );
